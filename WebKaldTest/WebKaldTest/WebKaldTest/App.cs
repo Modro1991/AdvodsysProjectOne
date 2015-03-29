f9a65ff73b240372ea57a3265d516a3fda52f73e
Modro <mah_57@hotmail.com>
2015-03-29 01:21:07 +0100

    Scroll in SagsLayout

--- WebKaldTest/WebKaldTest/WebKaldTest/App.cs
+++ WebKaldTest/WebKaldTest/WebKaldTest/App.cs
@@ -11,7 +11,7 @@ namespace WebKaldTest
     {
         public App()
         {
-			MainPage = new Menu();
+			MainPage = new Sagslayout();
 
 
         }
--- WebKaldTest/WebKaldTest/WebKaldTest/View/Menu.cs
+++ WebKaldTest/WebKaldTest/WebKaldTest/View/Menu.cs
@@ -53,6 +53,7 @@ namespace WebKaldTest
 					b3
 				}
 			};
+
 			
 		}
 	}
